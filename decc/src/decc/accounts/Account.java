e47918ac867827eea5f816e3e644709d018f8912
nyradr <nyradr@protonmail.com>
2016-05-31 13:14:51 +0200

    account use RSA instead of PGP.

--- decc/src/decc/accounts/Account.java
+++ decc/src/decc/accounts/Account.java
@@ -1,53 +1,14 @@
 package decc.accounts;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
-import java.security.SecureRandom;
-import java.util.Date;
-import java.util.Iterator;
+import java.security.PublicKey;
+import java.security.Security;
 
-import org.bouncycastle.bcpg.HashAlgorithmTags;
-import org.bouncycastle.bcpg.SymmetricKeyAlgorithmTags;
-import org.bouncycastle.bcpg.sig.Features;
-import org.bouncycastle.bcpg.sig.KeyFlags;
-import org.bouncycastle.crypto.generators.RSAKeyPairGenerator;
-import org.bouncycastle.crypto.params.RSAKeyGenerationParameters;
-import org.bouncycastle.openpgp.PGPCompressedData;
-import org.bouncycastle.openpgp.PGPCompressedDataGenerator;
-import org.bouncycastle.openpgp.PGPEncryptedData;
-import org.bouncycastle.openpgp.PGPEncryptedDataGenerator;
-import org.bouncycastle.openpgp.PGPException;
-import org.bouncycastle.openpgp.PGPKeyPair;
-import org.bouncycastle.openpgp.PGPKeyRingGenerator;
-import org.bouncycastle.openpgp.PGPLiteralData;
-import org.bouncycastle.openpgp.PGPLiteralDataGenerator;
-import org.bouncycastle.openpgp.PGPPrivateKey;
-import org.bouncycastle.openpgp.PGPPublicKey;
-import org.bouncycastle.openpgp.PGPPublicKeyRing;
-import org.bouncycastle.openpgp.PGPPublicKeyRingCollection;
-import org.bouncycastle.openpgp.PGPSecretKeyRing;
-import org.bouncycastle.openpgp.PGPSecretKeyRingCollection;
-import org.bouncycastle.openpgp.PGPSignature;
-import org.bouncycastle.openpgp.PGPSignatureGenerator;
-import org.bouncycastle.openpgp.PGPSignatureSubpacketGenerator;
-import org.bouncycastle.openpgp.PGPUtil;
-import org.bouncycastle.openpgp.operator.PBESecretKeyDecryptor;
-import org.bouncycastle.openpgp.operator.PBESecretKeyEncryptor;
-import org.bouncycastle.openpgp.operator.PGPContentSignerBuilder;
-import org.bouncycastle.openpgp.operator.PGPDigestCalculator;
-import org.bouncycastle.openpgp.operator.PGPDigestCalculatorProvider;
-import org.bouncycastle.openpgp.operator.bc.BcPBESecretKeyDecryptorBuilder;
-import org.bouncycastle.openpgp.operator.bc.BcPBESecretKeyEncryptorBuilder;
-import org.bouncycastle.openpgp.operator.bc.BcPGPContentSignerBuilder;
-import org.bouncycastle.openpgp.operator.bc.BcPGPDataEncryptorBuilder;
-import org.bouncycastle.openpgp.operator.bc.BcPGPDigestCalculatorProvider;
-import org.bouncycastle.openpgp.operator.bc.BcPGPKeyPair;
-import org.bouncycastle.openpgp.operator.bc.BcPublicKeyKeyEncryptionMethodGenerator;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
  * Represent a DECC account
@@ -56,9 +17,8 @@ import org.bouncycastle.openpgp.operator.bc.BcPublicKeyKeyEncryptionMethodGenera
 public class Account {
 	
 	private String name;
-	private PGPPublicKeyRing publicKey;
-	private PGPSecretKeyRing privateKeyRing;
-	private PGPPrivateKey privateKey;
+	private PublicKey pubkey;
+	private PrivateKey privkey;
 	
 	/**
 	 * Create account with private key (can decrypt)
@@ -67,15 +27,11 @@ public class Account {
 	 * @param prvk
 	 * @throws PGPException 
 	 */
-	public Account(String name, String pass, PGPPublicKeyRing pubk, PGPSecretKeyRing prvk) throws PGPException{
+	public Account(String name, PublicKey pubk, PrivateKey prvk){
 		this.name = name;
 		
-		publicKey = pubk;
-		privateKeyRing = prvk;
-		
-		BcPBESecretKeyDecryptorBuilder skdb = new BcPBESecretKeyDecryptorBuilder(new BcPGPDigestCalculatorProvider());
-		PBESecretKeyDecryptor skd = skdb.build(pass.toCharArray());
-		privateKey = privateKeyRing.getSecretKey().extractPrivateKey(skd);
+		pubkey = pubk;
+		privkey = prvk;
 	}
 	
 	/**
@@ -90,143 +46,34 @@ public class Account {
 	 * Get the account public PGP key
 	 * @return
 	 */
-	public PGPPublicKeyRing getPublic(){
-		return publicKey;
+	public PublicKey getPublic(){
+		return pubkey;
 	}
 	
 	/**
 	 * Get the account private PGP key
 	 * @return
 	 */
-	public PGPSecretKeyRing getPrivate(){
-		return privateKeyRing;
+	public PrivateKey getPrivate(){
+		return privkey;
 	}
 	
 	/**
-	 * Encrypt data with the account public key
-	 * @param data data to encrypt
-	 * @return
-	 * @throws PGPException 
-	 * @throws IOException 
-	 */
-	public String encrypt(String data) throws IOException, PGPException{
-		final int buff_size = 64;
-		InputStream is = new ByteArrayInputStream(data.getBytes());
-		OutputStream os = new ByteArrayOutputStream();
-		
-		// encryption
-		BcPGPDataEncryptorBuilder deb = new BcPGPDataEncryptorBuilder(PGPEncryptedData.AES_256);
-		deb.setSecureRandom(new SecureRandom());
-		deb.setWithIntegrityPacket(true);
-		
-		PGPEncryptedDataGenerator edg = new PGPEncryptedDataGenerator(deb);
-		edg.addMethod(new BcPublicKeyKeyEncryptionMethodGenerator(publicKey.getPublicKey()));
-		
-		OutputStream eos = edg.open(os, new byte[buff_size]);
-		
-		PGPCompressedDataGenerator cdg = new PGPCompressedDataGenerator(PGPCompressedData.ZIP);
-		OutputStream cos = cdg.open(eos);
-		
-		// signature
-		PGPContentSignerBuilder csb = new BcPGPContentSignerBuilder(
-				privateKeyRing.getSecretKey().getPublicKey().getAlgorithm(),
-				HashAlgorithmTags.SHA256);
-		PGPSignatureGenerator sg = new PGPSignatureGenerator(csb);
-		sg.init(PGPSignature.BINARY_DOCUMENT, privateKey);
-		
-		Iterator<String> it = privateKeyRing.getSecretKey().getUserIDs();
-		if(it.hasNext()){
-			PGPSignatureSubpacketGenerator ssg = new PGPSignatureSubpacketGenerator();
-			ssg.setSignerUserID(false, it.next());
-			sg.setHashedSubpackets(ssg.generate());
-		}
-		
-		sg.generateOnePassVersion(false).encode(cos);
-		
-		PGPLiteralDataGenerator ldg = new PGPLiteralDataGenerator();
-		OutputStream los = ldg.open(cos, PGPLiteralData.BINARY, "data", new Date(), new byte[buff_size]);
-		
-		// encrypt and sign
-		byte[] buff = new byte[buff_size];
-		int len = 0;
-		
-		while((len = is.read(buff)) > 0){
-			los.write(buff);
-			sg.update(buff, 0, len);
-		}
-		is.close();
-		
-		sg.generate().encode(cos);
-		cdg.close();
-		edg.close();
-		
-		String ret = os.toString();
-		os.close();
-		
-		return ret;
-	}
-	
-	/**
-	 * Decrypt data with the account private key
-	 * @param data data to decrypt
-	 * @return decrypted data
-	 */
-	public String decrypt(String data){
-		return "";
-	}
-	
-	/**
-	 * Create new account and generate PGP key pair
+	 * Create new account and generate key pair
 	 * @param name user name
-	 * @param pass PGP password
-	 * @param size key size
-	 * @return
-	 * @throws PGPException 
+	 * @param size RSA key size
+	 * @return account
+	 * @throws NoSuchProviderException 
+	 * @throws NoSuchAlgorithmException 
 	 */
-	public static Account create(String name, String pass, int size) throws PGPException{
-		// rsa key generation
-		RSAKeyPairGenerator kpg = new RSAKeyPairGenerator();
-		kpg.init(new RSAKeyGenerationParameters(
-				BigInteger.valueOf(0x10001), new SecureRandom(), size, 12
-				));
+	public static Account create(String name, int size) throws NoSuchAlgorithmException, NoSuchProviderException{
+		Security.addProvider(new BouncyCastleProvider());
 		
-		// key creation
-		PGPKeyPair key = new BcPGPKeyPair(PGPPublicKey.RSA_GENERAL, kpg.generateKeyPair(), new Date());
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "BC");
+		kpg.initialize(size);
 		
-		// key signature creation
-		PGPSignatureSubpacketGenerator sign = new PGPSignatureSubpacketGenerator();
-		sign.setKeyFlags(false, KeyFlags.SHARED);
-		sign.setPreferredHashAlgorithms(false, new int[]{
-				HashAlgorithmTags.SHA512,
-				HashAlgorithmTags.SHA384,
-				HashAlgorithmTags.SHA256
-		});
-		sign.setPreferredSymmetricAlgorithms(false, new int[]{
-				SymmetricKeyAlgorithmTags.AES_256,
-				SymmetricKeyAlgorithmTags.TWOFISH,
-				SymmetricKeyAlgorithmTags.BLOWFISH
-		});
-		sign.setFeature(false, Features.FEATURE_MODIFICATION_DETECTION);
+		KeyPair kp = kpg.generateKeyPair();
 		
-		// hashs
-		PGPDigestCalculator sha1c = new BcPGPDigestCalculatorProvider()
-				.get(HashAlgorithmTags.SHA1);
-		PGPDigestCalculator sha256c = new BcPGPDigestCalculatorProvider()
-				.get(HashAlgorithmTags.SHA256);
-		
-		// private key encryption
-		PBESecretKeyEncryptor ske = new BcPBESecretKeyEncryptorBuilder(
-				PGPEncryptedData.AES_256, sha256c, 0x60)
-				.build(pass.toCharArray());
-		
-		// create key ring
-		PGPKeyRingGenerator krg = new PGPKeyRingGenerator(
-				PGPSignature.POSITIVE_CERTIFICATION, key,
-				name, sha1c, sign.generate(), null,
-				new BcPGPContentSignerBuilder(key.getPublicKey().getAlgorithm(), HashAlgorithmTags.SHA1),
-				ske);
-				
-		// create account
-		return new Account(name, pass, krg.generatePublicKeyRing(), krg.generateSecretKeyRing());
+		return new Account(name, kp.getPublic(), kp.getPrivate());
 	}
 }
